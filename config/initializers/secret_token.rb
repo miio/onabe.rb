4d7c9e9f4c445f1e5a0a6fa901221d4f67c563b8
miio <info@miio.info>
2011-11-25 00:32:56 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nabe::Application.config.secret_token = '7968a8f8dca8f90a318206b2df74cb97663fe6b46ff38d1f1fd3aa1289934bf9894cfdc5e549aaf3b9f82c6b98c5c2d9f09a5d09de228e40e0e1ba55203f3ac8'

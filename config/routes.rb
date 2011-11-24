f9dd4ca49981fe6bafc1cfe094aeecae0e7ce934
miio <info@miio.info>
2011-11-25 01:08:58 +0900

    install active admin base

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Nabe::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
+  resources :asserts
+
+  resources :events
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

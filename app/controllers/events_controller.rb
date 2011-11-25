2b20f6cf0e2364347d270cc436ae730dca2005a5
miio <info@miio.info>
2011-11-25 21:23:19 +0900

    add asserts for event detail

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,7 +14,7 @@ class EventsController < ApplicationController
   # GET /events/1.json
   def show
     @event = Event.find(params[:id])
-
+    @asserts = Assert.where(:event_id => params[:id])
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @event }
